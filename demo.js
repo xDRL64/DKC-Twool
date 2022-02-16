db85c76c91b8c5ddaf87f6ffe3b3bb84dcae58cc
DRL <26519878+xDRL64@users.noreply.github.com>
2022-02-17 00:08:19 +0100

    added file : demo.js

---
+++ demo.js
@@ -0,0 +1,29 @@
+
+(function(app=dkc2ldd){
+
+    let params = new URLSearchParams(location.search);
+    let p = params;
+
+    if(p.has('preload') && p.get('preload')==='rom'){
+        app.interface.srcFilePanel.rom.set_oneDataFile(
+            {name:'decompressed U1.0 rom.smc', data:dkc2debug.preload.decompressedRom, useDec:false}, true
+        );
+    }
+
+    if(p.has('demo') && p.get('demo')==='animated'){
+
+        // set file data
+        app.interface.update_workspace('mode_');
+        
+        // show mapchip with animated tileset
+        app.interface.srcFilePanel.mapchip.parameters.value = "vram=1";
+        app.interface.update_workspace('mode2');
+
+        // hide side panels
+        let fakeEvent = {stopPropagation:function(){},preventDefault:function(){}};
+        app.event.slide_panel(fakeEvent, app.interface.srcFilePanel);
+        app.event.slide_panel(fakeEvent, app.interface.editModePanel);
+    
+    }
+
+})();
