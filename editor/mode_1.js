8244661398a36dbcba1d2dcb279450f2f39d0bbe
DRL <26519878+xDRL64@users.noreply.github.com>
2022-06-16 01:06:15 +0200

    mode_1.js : update

--- editor/mode_1.js
+++ editor/mode_1.js
@@ -11,6 +11,11 @@
 		let wLib = app.editor;
 		let wLib2 = app.WorkspaceToolPack;
 
+		let Elem = wLib2.CreateElem;
+		let flags = Elem.flags;
+		for(let key in flags)
+			eval(`if(!${key}) var ${key} = ${flags[key]}`);
+
 		// empty workspace (to empty html child elements)
 		workspace.elem.textContent = "";
 
@@ -24,7 +29,7 @@
 
 		let romOfst = 0;
 
-		slot.parameters.onkeydown = function(e){
+		/* slot.parameters.onkeydown = function(e){
 			if(e.code === 'Enter') o.update();
 			if(e.code === 'ArrowUp'){
 				romOfst--;
@@ -34,116 +39,298 @@
 				romOfst++;
 				 o.update();
 			}
+		}; */
+
+		let _xtmax = 16;
+		slot.parameters.onkeydown = function(e){
+			if(e.code === 'Enter'){
+				let parameters = slot.parameters.value.match(/\w{1,}/g) || [];
+				_xtmax = parseInt(parameters[0]) || 16;
+				render();
+			}
 		};
 
-		o.mode_1 = document.createElement('div');
-		o.mode_1.style.position = 'relative';
+		let _bpp = 4;
+		let _scale = 1;
+		let _tlstSel = 'none';
+		let _data = null;
+		let _romStart = 0;
+		let _romSize = 0;
+		let defPal = app.gfx.defaultSnespal.concat(app.gfx.debugSnespal);
+		let propset;
 
-		let settingsPanel = document.createElement('div');
-		settingsPanel.style.position = 'absolute';
-		settingsPanel.style.width = '20%';
-		settingsPanel.style.height = '100%';
+		o.mode_1 = Elem('div', {disFlx, width:'100%', height:'100%'});
 
-		let selectSrcFile = document.createElement('select');
+			//propset = {width:384, height:'100%', disFlx, fdrCln, atmCen};
+			let leftPanel = Elem('div', {disFlx, fdrCln, atmCen});
 
-		//let selectBpp = document.createElement('select');
-		let selectBpp = wLib2.DropList('bpp : ');
-		for(let i=2; i<9; i *= 2){
-			/* let op = document.createElement('option');
-			op.textContent = i+'bpp';
-			op.setAttribute('value', i);
-			selectBpp.appendChild(op); */
-			selectBpp.generate_item(i,i);
-		}
-		selectBpp.elem.value = 4;
-		o.mode_1.appendChild(selectBpp.board);
+				let settingsPanel = Elem('div', {w:'100%', h:'fit-content', disFlx, fdrCln, atmCen});
 
-		settingsPanel.appendChild(selectSrcFile);
-		o.mode_1.appendChild(settingsPanel);
+					
+					// BPP LIST
+					let selectBpp = wLib2.DropList('bpp : ');
+					for(let i=2; i<9; i *= 2)
+						selectBpp.generate_item(i,i);
+					Elem.Set(selectBpp.elem, {_value:_bpp, _size:3, w:48});
+					selectBpp.elem.children[_bpp>>2].setAttribute('selected', true);
+					Elem.Set(selectBpp.board, {margin:'16 0'});
 
-		let renderSpace = document.createElement('div');
-		renderSpace.style.position = 'absolute';
-		renderSpace.style.width = '80%';
-		renderSpace.style.height = '100%';
-		renderSpace.style.right = 0;
-		o.mode_1.appendChild(renderSpace);
+					// SRC LIST
+					let selectSrcFile = wLib2.DropList('src : ');
+					Elem.Set(selectSrcFile.elem, {_size:5, w:'100%'});
+					Elem.Set(selectSrcFile.board, {disFlx, fdrCln, alnItm:'start', w:256, margin:'16 0'});
 
+					// SRC ROM SETTINGS
+					let create_HexInputWithStep = function(name=''){
+						let panel = Elem('div', {w:256});
+						let label = Elem('label', {w:'100%', txtCnt:name})
+							let hexInput = wLib2.HexInput('0x', true, ['DATA_']);
+							let stepInput = wLib2.HexInput('0x', true, ['DATA_']);
+							Elem.Set(hexInput, {w:'70%'});
+							Elem.Set(stepInput, {w:'30%'});
+							stepInput.onblur = function(){hexInput.step=this.int};
+						panel.appendChild(label);
+						panel.appendChild(hexInput);
+						panel.appendChild(stepInput);
+						return {board:panel, hexInput};
+					};
+					let romOffsetPan = create_HexInputWithStep('rom display offset :');
+					let romDisplayPan = create_HexInputWithStep('rom display size :');
+
+				settingsPanel.appendChild(selectBpp.board);
+				settingsPanel.appendChild(selectSrcFile.board);
+				settingsPanel.appendChild(romOffsetPan.board);
+				settingsPanel.appendChild(romDisplayPan.board);
+
+				// PALETTE PICKER
+				propset = {disFlx, fdrCln, atmCen, ovyScl, w:'100%', padding:16, bxszbb};
+				let palSelectorPan = Elem('div', propset)
+					
+					let get_safeSnespal = function(snespal){
+						/* //snespal = srcFilePanel.palette.get_data__OLD().jsArray?.() || defPal;
+						snespal = srcFilePanel.palette.get_data__OLD() || defPal;
+						snespal = snespal?.jsArray ? snespal.jsArray() : snespal;
+						if(snespal.length < 512) snespal = snespal.concat(defPal);
+						return snespal; */
+						return srcFilePanel.palette.get_allInOne(false).concat(defPal);
+					};
+					let snespal = get_safeSnespal();
+					let palSelector = wLib.create_snesPaletteSelector(snespal, _bpp);
+				palSelectorPan.appendChild(palSelector.elem);
+
+			leftPanel.appendChild(settingsPanel);
+			leftPanel.appendChild(palSelectorPan);
+
+			let rightPanel = Elem('div', {ovfHid, flxGrw:1, disFlx, fdrCln, jtcCen, atmCen});
+
+				let renderScaleInfo = Elem('div', {txtCnt:`Render Scale : ${_scale}`});
+
+				let scaleRange = Elem('input', {_type:'range', _value:1, _min:1, _max:5, _step:1});
+
+				let renderSpace = Elem('div', {w:'100%', ovfScl, bxszbb ,flxGrw:1, disFlx, fdrCln, jtcCen, atmCen});
+				renderSpace.style.border = '1px solid';
+
+				let decompPrevPan = Elem('div', {w:'100%', ovfScl, bxszbb, disFlx});
+
+			rightPanel.appendChild(renderScaleInfo);
+			rightPanel.appendChild(scaleRange);
+			rightPanel.appendChild(renderSpace);
+			rightPanel.appendChild(decompPrevPan);
+
+		o.mode_1.appendChild(leftPanel);
+		o.mode_1.appendChild(rightPanel);
 		workspace.elem.appendChild(o.mode_1);
 
-		selectBpp.elem.onchange =
-		selectSrcFile.onchange =
-		function(){
-			update_render();
+		// EVENT CONNEXIONS
+
+		selectBpp.elem.onchange = function(){
+			_bpp = parseInt(this.value);
+			update_palettes();
+			render();
+		};
+		selectSrcFile.elem.onchange = function(){
+			_tlstSel = selectSrcFile.elem.value;
+			update_tileset();
+			render();
 		};
 
-		let update_render = function(){
+		let romSettingChange = function(){
+			_romStart = romOffsetPan.hexInput.int;
+			_romSize = romDisplayPan.hexInput.int;
+			update_tileset();
+			render();
+		};
+		romOffsetPan.hexInput.onkeyup = function(e){romSettingChange(e)};
+		romDisplayPan.hexInput.onkeyup = function(e){romSettingChange(e)};
 
-			let data = null;
+		scaleRange.oninput = function(){
+			_scale = this.value;
+			renderScaleInfo.textContent = `Render Scale : ${_scale}`;
+			render();
+		};
 
+		renderSpace.ondblclick = function(){
+			let romRef = {address:_romStart, _romSize};
+			let defFile = {name:'from mode_1', data:_data, useDec:false, romRef, vramRef:{bpp:_bpp}};
+			slot.set_oneDataFile(defFile, true);
+
+			// update
+			o.update('tileset');
+		};
+
+		let update_srcList = function(){
+			// add fill src list options
+			selectSrcFile.elem.innerHTML = '';
+			let len = slot.multi;
+			for(let i=0; i<len; i++){
+				selectSrcFile.generate_item(slot.names[i], i);
+			}
+			// add 'ALL IN ONE' option
+			if(len){
+				selectSrcFile.generate_item('ALL IN ONE', 'all');
+			}
+			// add 'FROM ROM' option
+			if(srcFilePanel.check_slot('rom')){
+				selectSrcFile.generate_item('FROM ROM', 'rom');
+			}
+		};
+
+		
+		let update_tileset = function(){
 			// get src file
-			let val = selectSrcFile.value;
-			if(val === 'all'){
-				data = slot.get_data__OLD();
-			}else if(val === 'rom'){
-				data = srcFilePanel.rom.fileData[0];
+			if(_tlstSel === 'all'){
+				_data = slot.get_allInOne(false); // get js Array
+			}else if(_tlstSel === 'rom'){
+				let end = _romStart + _romSize;
+				_data = srcFilePanel.rom.fileData[0].slice(_romStart, end); // get Uint8Array
 			}else{
-				let index = parseInt(val) || 0;
-				data = slot.get_data()[index];
+				let index = parseInt(_tlstSel) || 0;
+				_data = slot.get_data()[index]; // get Uint8Array
 			}
 
+			// if tileset is null
+			let safeDefault = new Uint8Array(64);
+			_data ??= safeDefault;
+
+			// if tileset is empty or not big enough
+			if(_data.length < 64)
+				_data = ([..._data]).concat([...safeDefault]);
+			
+		};
+
+		let update_palettes = function(){
+			snespal = get_safeSnespal();
+			let newPalSel = wLib.create_snesPaletteSelector(snespal, _bpp);
+			palSelectorPan.replaceChild(newPalSel.elem, palSelector.elem);
+			palSelector = newPalSel;
+			palSelector.callback = function(){ render(); };
+		};
+
+		let update_decompPreviews = function(w,h, s, pal, xtmax){
+
+			decompPrevPan.innerHTML = '';
+
+			let tileByteSize = _bpp << 3; // mul by 8
+
+			let sample = null;
+			let size = _data.length;
+
+			let make_preview = function(text, data){
+				let elem = Elem('div', {margin:8});
+					let txt = Elem('label', {txtCnt:text});
+					let viewport = wLib.create_preview(w,h, s);
+					Elem.Set(viewport.elem, {margin:8});
+					let ctx = viewport.ctx;
+					app.gfx.fast[`draw_${_bpp}bppTileset`](data, pal, 0,0, xtmax, ctx);
+				elem.appendChild(txt);
+				elem.appendChild(viewport.elem);
+				return elem;
+			};
+
+			for(let i=0; i<tileByteSize; i++){
+				sample = app.decompressor( _data.slice(i, size) );
+				sample = new Uint8Array( ([...sample]).concat([...(new Uint8Array(64))]) );
+				decompPrevPan.appendChild( make_preview(i, sample) );
+			}
+
+		};
+
+		let render = function(){
 			// render
-			let bpp = selectBpp.elem.value;
-			let bppSizeBitShift = ({2:4,4:5,8:6})[bpp];
-			let xtmax = 16;
-			let h = Math.ceil( (data.length>>bppSizeBitShift) / xtmax ) << 3;
+			//let bpp = selectBpp.elem.value;
+			let bppSizeBitShift = ({2:4,4:5,8:6})[_bpp];
+			let xtmax = _xtmax;    // div by 16/32/64
+			let h = Math.ceil( (_data.length>>bppSizeBitShift) / xtmax ) << 3; // mul by 8
 			let w = xtmax << 3;
-			let s = 1;
 		
-			let viewport = wLib.create_preview(w,h, s);
+			// make viewport
+			let viewport = wLib.create_preview(w,h, _scale);
+			Elem.Set(viewport.elem, {margin:16});
 			let ctx = viewport.ctx;
 			renderSpace.innerHTML = '';
 			renderSpace.appendChild(viewport.elem);
 
-			let pal = app.gfx.defaultPalettes[1];
-			//app.gfx[_gfx].draw_4bppTileset(data, pal, 0,0, 16, ctx);
-			app.gfx.fast[`draw_${bpp}bppTileset`](data, pal, 0,0, xtmax, ctx);
+			// viewport placing update
+			let prevPanHeight = renderSpace.getBoundingClientRect().height;
+			let prevHeight = parseInt(getComputedStyle(viewport.elem).height);
+			renderSpace.style.justifyContent = prevPanHeight<=prevHeight ? 'start' : 'center';
+			let prevPanWidth = renderSpace.getBoundingClientRect().width;
+			let prevWidth = parseInt(getComputedStyle(viewport.elem).width);
+			renderSpace.style.alignItems = prevPanWidth<=prevWidth ? 'start' : 'center';
 
+			// get palette
+			let pal = palSelector.palette;
+			pal = !pal.length ? app.gfx.defaultPalettes[1] : pal;
+
+			//app.gfx[_gfx].draw_4bppTileset(_data, pal, 0,0, 16, ctx);
+			app.gfx.fast[`draw_${_bpp}bppTileset`](_data, pal, 0,0, xtmax, ctx);
+
+			// decompressed previews panel update
+			let decompPrevFactor = 0.5, dpf = decompPrevFactor;
+			let floor = Math.floor;
+			let max = Math.max;
+			update_decompPreviews(
+				max(floor(w*dpf), 1),
+				max(floor(h/dpf), 1),
+				max(floor(_scale*dpf), 1),
+				pal,
+				max(floor(xtmax*dpf), 1)
+			)
 		};
 
 		// update
 		o.update = function(trigger){
 
-			if(trigger==='reset' || trigger==='tileset' || trigger==='rom'){
-				// fill src list
-				selectSrcFile.innerHTML = '';
-				let len = slot.multi;
-				let file;
-				for(let i=0; i<len; i++){
-					file = document.createElement('option');
-					file.textContent = slot.names[i];
-					file.setAttribute('value', i);
-					selectSrcFile.appendChild(file);
-				}
-				// add 'ALL IN ONE' option
-				if(len){
-					file = document.createElement('option');
-					file.textContent = 'ALL IN ONE';
-					file.setAttribute('value', 'all');
-					selectSrcFile.appendChild(file);
-				}
-				// add 'FROM ROM' option
-				if(srcFilePanel.check_slot('rom')){
-					file = document.createElement('option');
-					file.textContent = 'FROM ROM';
-					file.setAttribute('value', 'rom');
-					selectSrcFile.appendChild(file);
-				}
+			if(trigger==='reset'){
+				update_srcList();
+				update_tileset();
+				update_palettes();
+				render();
+			}
+			
+			if(trigger==='tileset'){
+				update_srcList();
+				update_tileset();
+				render();
 			}
 
+			if(trigger==='rom'){
+				update_srcList();
+				update_tileset();
+				render();
+			}
 
-
+			if(trigger==='palette'){
+				update_palettes();
+				render();
+			}
 			
 
+
+
+
+			// OLD SYSTEM
+			if(false)
 			if(slot.multi > 0){
 
 				let data = slot.get_data__OLD();
